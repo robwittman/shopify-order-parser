e9d9005b46bbe478126e9701f8b6a281818b8c04
Rob Wittman <rob@ihsdigital.com>
2017-03-30 17:32:34 -0400

    Require login

--- src/Middleware/Authorization.php
+++ src/Middleware/Authorization.php
@@ -7,7 +7,7 @@ class Authorization
     public function __invoke($request, $response, $next)
     {
         if (!isset($_SESSION['uid'])) {
-            // return $response->withRedirect('/login');
+            return $response->withRedirect('/login');
         }
 
         return $next($request, $response);
